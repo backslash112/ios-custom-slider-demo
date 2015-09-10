a8d10b5ad6dd7b9720a79b0ec4bf50bf2f62385e
backslash112 <yangcun@live.com>
2015-09-10 10:37:17 +0800

    first commit

---
+++ ios-custom-slider-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-custom-slider-demo
+//
+//  Created by Carl.Yang on 9/10/15.
+//  Copyright © 2015 Yang Cun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
